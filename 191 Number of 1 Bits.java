2a98aa0fb25fb9386eecde3bd59b90caf89ea303
Yue <yuz82@pitt.edu>
2015-09-22 17:34:39 -0400

    Create 191 Number of 1 Bits.java

---
+++ 191 Number of 1 Bits.java
@@ -0,0 +1,20 @@
+/*
+Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
+For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+*/
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        String str = Integer.toBinaryString(n);
+        int len = str.length();
+        int count = 0;
+        for(int i=0;i<len;i++){
+            if(str.charAt(i)=='1'){
+                count++;
+            }
+        }
+        return count;
+    }
+ 
+}
