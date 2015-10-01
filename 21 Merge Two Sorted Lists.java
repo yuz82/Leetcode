1e214eb7e747ebb809ee9d451f8317572db5dda3
Yue <yuz82@pitt.edu>
2015-10-01 18:05:20 -0400

    recursive

--- 21 Merge Two Sorted Lists.java
+++ 21 Merge Two Sorted Lists.java
@@ -40,3 +40,47 @@ public class Solution {
     }
 }
 
+
+//recursive
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1==null){
+            return l2;
+        }
+        if(l2==null){
+            return l1;
+        }
+        
+        ListNode head = new ListNode(0);
+        if(l1.val < l2.val){
+            head = l1;
+            head.next = mergeTwoLists(l1.next, l2);
+        }else{
+            head = l2;
+            head.next = mergeTwoLists(l1, l2.next);
+        }
+        return head;
+    }
+}
+
+
+
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1==null){
+            return l2;
+        }
+        if(l2==null){
+            return l1;
+        }
+        if(l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }else{
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
+
+
