1ac7c7b07fafaf4187de09613a901884e05f504b
Yue <yuz82@pitt.edu>
2015-11-07 17:37:47 -0500

    bfs

--- Minimum Depth of Binary Tree.java
+++ Minimum Depth of Binary Tree.java
@@ -43,3 +43,35 @@ public class Solution {
     }
 
 }
+
+//bfs
+public class Solution {
+    
+    public int minDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        TreeNode endOfLevel = root;
+        int depth = 1;
+        while(!queue.isEmpty()){
+            TreeNode node = queue.remove();
+            if(node.left==null&&node.right==null){
+                return depth;
+            }
+            if(node.left!=null){
+                queue.add(node.left);
+            }
+            if(node.right!=null){
+                queue.add(node.right);
+            }
+            if(node==endOfLevel){
+                depth++;
+                endOfLevel = (node.right==null?node.left:node.right);
+            }
+        }
+        return depth;
+    }
+
+}
