28b82d9b6b14ef46000803d1354a76c3c5d3e8ca
Yue <yuz82@pitt.edu>
2015-11-07 17:22:47 -0500

    dfs

--- Minimum Depth of Binary Tree.java
+++ Minimum Depth of Binary Tree.java
@@ -7,7 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
- 
+ //iteration
 public class Solution {
     public int minDepth(TreeNode root) {
         if(root==null){
@@ -16,6 +16,30 @@ public class Solution {
         int left = minDepth(root.left);
         int right = minDepth(root.right);
         return (left==0 || right==0) ? left+right+1 : Math.min(left,right)+1;
+    }
+
+}
+//DFS
+public class Solution {
+    int min = Integer.MAX_VALUE;
+    public int minDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        dfs(root,1);
+        return min;
+    }
+    
+    public void dfs(TreeNode root, int depth){
+        if(root.left!=null){
+            dfs(root.left, depth+1);
+        }
+        if(root.right!=null){
+            dfs(root.right, depth+1);
+        }
+        if(root.left==null&&root.right==null){
+            min = Math.min(min, depth);
+        }
     }
 
 }
