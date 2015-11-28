02ee757a0bdd92779af1ef12a278290ea58b542b
Yue <yuz82@pitt.edu>
2015-11-28 17:18:12 -0500

    Create 189 Rotate Array.java

---
+++ 189 Rotate Array.java
@@ -0,0 +1,26 @@
+/*
+Rotate an array of n elements to the right by k steps.
+
+For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+
+Note:
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+*/
+
+//O(n) space
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int offset = k%n;
+        if(offset!=0){
+            int[] arr = Arrays.copyOf(nums, n); 
+            for(int i=0;i<n;i++){
+                if(i+offset<n){
+                    nums[i+offset] = arr[i];
+                }else{
+                    nums[i+offset-n] = arr[i];
+                }
+            }
+        }
+    }
+}
