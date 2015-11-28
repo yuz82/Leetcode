d165e435310a24e0ec40e0db1c5f3bc3567e11b4
Yue <yuz82@pitt.edu>
2015-11-28 15:17:53 -0500

    Update 205 Isomorphic Strings.java

--- 205 Isomorphic Strings.java
+++ 205 Isomorphic Strings.java
@@ -36,3 +36,15 @@ public class Solution {
         return true;
     }
 }
+
+
+public class Solution {
+    public boolean isIsomorphic(String s1, String s2) {
+        int[] m = new int[512]; //record the last appeared position of each letter
+        for (int i = 0; i < s1.length(); i++) {
+            if (m[s1.charAt(i)] != m[s2.charAt(i)+256]) return false;
+            m[s1.charAt(i)] = m[s2.charAt(i)+256] = i+1;
+        }
+        return true;
+    }
+}
