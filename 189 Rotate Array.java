4d7e2f254a22294c8f314c48fdc6efd4c084f821
Yue <yuz82@pitt.edu>
2015-11-29 22:15:50 -0500

    Update 189 Rotate Array.java

--- 189 Rotate Array.java
+++ 189 Rotate Array.java
@@ -24,3 +24,22 @@ public class Solution {
         }
     }
 }
+
+//O(1) space  reverse
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int offset = k%n;
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, offset-1);
+        reverse(nums, offset, n-1);
+    }
+    
+    public void reverse(int[] nums, int start, int end){
+        while(start<end){
+            int tmp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = tmp;
+        }
+    }
+}
