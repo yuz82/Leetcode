644ba44c0c9e757ff606fbe2035d0d5389f830a1
Yue <yuz82@pitt.edu>
2015-12-13 16:23:58 -0500

    Create NumGame.java

---
+++ NumGame.java
@@ -0,0 +1,26 @@
+public static void numgame(int a, int b) {
+        HashSet<Integer> map = new HashSet<>();
+        map.add(a);
+        map.add(b);
+        boolean endmark = true;
+        while (endmark) {
+            endmark = false;
+            for (int num1 : map) {
+                for (int num2 : map) {
+                    int next = Math.abs(num1 - num2);
+                    if (!map.contains(next) && next > 0) {
+                        System.out.print(next+" ");
+                        map.add(next);
+                        endmark = true;
+                        break;
+                    }
+                }
+                if(endmark) break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        numgame(20, 2);
+        //18 16 14 4 12 10 6 8
+    }
