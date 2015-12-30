239c195e0e30b031fe51f22f49078a82752fc771
Yue <yuz82@pitt.edu>
2015-12-30 16:39:28 -0500

    Update 299 Bulls and Cows.java

--- 299 Bulls and Cows.java
+++ 299 Bulls and Cows.java
@@ -39,3 +39,19 @@ public class Solution {
         return bull+"A"+cow+"B";
     }
 }
+
+
+//solution 2  one-pass
+public String getHint(String secret, String guess) {
+    int bulls = 0;
+    int cows = 0;
+    int[] numbers = new int[10];
+    for (int i = 0; i<secret.length(); i++) {
+        if (secret.charAt(i) == guess.charAt(i)) bulls++;
+        else {
+            if (numbers[secret.charAt(i)-'0']++ < 0) cows++;
+            if (numbers[guess.charAt(i)-'0']-- > 0) cows++;
+        }
+    }
+    return bulls + "A" + cows + "B";
+}
