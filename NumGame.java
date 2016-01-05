26beb65a05f45d0592b872ee6e3dfd4033c27d18
Yue <yuz82@pitt.edu>
2016-01-05 14:58:54 -0500

    Update NumGame.java

--- NumGame.java
+++ NumGame.java
@@ -1,5 +1,7 @@
 /*
 the numbers should equal to the difference of existed numbers in the array and the numbers in the array should keep distinct
+
+hint: find the maximum common factor
 */
 
 public static void numgame(int a, int b) {
@@ -27,4 +29,28 @@ public static void numgame(int a, int b) {
     public static void main(String[] args) {
         numgame(20, 2);
         //18 16 14 4 12 10 6 8
+    }
+    
+    
+    //find max common factor
+     public static void numGame2(int num1, int num2){
+       int mcd = gcd(num1, num2);
+       int max = num1>num2?num1:num2;
+       while(true){
+            max = max - mcd;
+            if(max>0){
+               System.out.print(max+" ");
+               
+           }else{
+                break;
+            }
+       }
+   }
+   
+    public static int gcd(int num1, int num2) {
+        return num2==0 ? num1 : gcd(num2, num1%num2);
+    }
+
+    public static void main(String[] args) {
+        numGame2(20, 2);
     }
