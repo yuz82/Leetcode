51c91c0c3b215b809cc0e3b7d690f93a284c6dde
Yue <yuz82@pitt.edu>
2016-01-05 14:05:28 -0500

    Update 53 Maximum Subarray.java

--- 53 Maximum Subarray.java
+++ 53 Maximum Subarray.java
@@ -18,3 +18,21 @@ public class Solution {
     }
 }
 
+//dynamic programming
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        if(nums==null || n==0){
+            return 0;
+        }
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        int max = dp[0];
+        for(int i=1;i<n;i++){
+            dp[i] = Math.max(dp[i-1] + nums[i], nums[i]);
+            max = Math.max(dp[i], max);
+        }
+        return max;
+    }
+}
+
