67ccced8e232feb43b6647e834c49b98f3be2fcf
Yue <yuz82@pitt.edu>
2016-01-09 14:15:01 -0500

    Create 326. Power of Three.java

---
+++ 326. Power of Three.java
@@ -0,0 +1,37 @@
+/*
+Given an integer, write a function to determine if it is a power of three.
+
+Follow up:
+Could you do it without using any loop / recursion?
+*/
+
+//loop
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        double num = (double) n;
+        while(Math.abs(num)>2){
+            num = num / 3;
+        }
+        if(num == 1){
+            return true;
+        }
+        return false;
+    }
+}
+
+//recursive
+public boolean isPowerOfThree(int n) {
+    return n>0 && (n==1 || (n%3==0 && isPowerOfThree(n/3)));
+}
+
+
+//The idea is to convert the original number into radix-3 format and check if it is of format 10* where 0* means k zeros with k>=0.
+public boolean isPowerOfThree(int n) {
+    return Integer.toString(n, 3).matches("10*");
+}
+
+
+public boolean isPowerOfThree(int n) {
+    return (Math.log10(n) / Math.log10(3)) % 1 == 0;
+}
+
