95e1b4977f84f056a0c895f0a41e19ab76e1e566
Yue <yuz82@pitt.edu>
2016-01-21 16:02:07 -0500

    Update 315. Count of Smaller Numbers After Self.java

--- 315. Count of Smaller Numbers After Self.java
+++ 315. Count of Smaller Numbers After Self.java
@@ -12,7 +12,12 @@ To the right of 1 there is 0 smaller element.
 Return the array [2, 1, 1, 0].
 */
 
-
+//note: not distinct number
+/*
+solution:  binary search
+Traverse from the end of this array, for each element, using binary search to find the location(return value plus one then negate it) to insert in a new empty array, the position to insert is the number of smaller numbers after itself.
+However, if using int[] to store the new array, although it has binarySearch() to simplify the code, the cost of insertion is hugh. Therefore, a binary search function implemented by arraylist is needed.
+*/
 
 //time limit exceed 
 public class Solution {
