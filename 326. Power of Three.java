b7384d5884b7a0fa66a8bf805cd74f10941a4d75
Yue <yuz82@pitt.edu>
2016-01-22 12:49:49 -0500

    Update 326. Power of Three.java

--- 326. Power of Three.java
+++ 326. Power of Three.java
@@ -19,6 +19,22 @@ public class Solution {
     }
 }
 
+//loop2
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n<=0){
+            return false;
+        }
+        while(n%3==0){
+            n /= 3;
+        }
+        if(n!=1){
+            return false;
+        }
+        return true;
+    }
+}
+
 //recursive
 public boolean isPowerOfThree(int n) {
     return n>0 && (n==1 || (n%3==0 && isPowerOfThree(n/3)));
