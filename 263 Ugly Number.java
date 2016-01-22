1d833bc79443e6c9984604b7cc1e874da57fdb22
Yue <yuz82@pitt.edu>
2016-01-22 12:30:01 -0500

    Update 263 Ugly Number.java

--- 263 Ugly Number.java
+++ 263 Ugly Number.java
@@ -32,3 +32,29 @@ for (int i=2; i<6 && num>0; i++)
     while (num % i == 0)
         num /= i;
 return num == 1;
+
+
+public class Solution {
+    public boolean isUgly(int num) {
+        if(num<1){
+            return false;
+        }
+        if(num == 1){
+            return true;
+        }
+        while(num!=1){
+            if(num%2==0){
+                num /= 2;
+                continue;
+            }else if(num%3==0){
+                num /= 3;
+                continue;
+            }else if(num%5==0){
+                num /= 5;
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
+}
