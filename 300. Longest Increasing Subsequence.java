72ec10e437f0a84903dde98a4d2e630dcfcbc87f
Yue <yuz82@pitt.edu>
2016-01-24 13:58:36 -0500

    Update 300. Longest Increasing Subsequence.java

--- 300. Longest Increasing Subsequence.java
+++ 300. Longest Increasing Subsequence.java
@@ -35,3 +35,21 @@ public class Solution {
         return max;
     }
 }
+
+
+//O(nlogn) binary search
+public class Solution {
+    public int lengthOfLIS(int[] nums) {            
+        int[] dp = new int[nums.length];
+        int len = 0;
+
+        for(int x : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, x);
+            if(i < 0) i = -(i + 1);
+            dp[i] = x;
+            if(i == len) len++;
+        }
+
+        return len;
+    }
+}
