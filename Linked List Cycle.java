5a78e1d5a9a2a185dc28b510d528bba7078f8a6c
Yue <yuz82@pitt.edu>
2016-01-25 12:09:23 -0500

    Update Linked List Cycle.java

--- Linked List Cycle.java
+++ Linked List Cycle.java
@@ -20,12 +20,12 @@ Can you solve it without using extra space?
 public class Solution {
     public boolean hasCycle(ListNode head) {
         if(head==null){ return false;}
-        ListNode pre = head;
-        ListNode cur = head;
-        while(cur.next!=null&&cur.next.next!=null){
-            pre = pre.next;
-            cur = cur.next.next;
-            if(pre == cur){
+        ListNode walker = head;
+        ListNode runner = head;
+        while(runner.next!=null&&runner.next.next!=null){
+            walker = walker.next;
+            runner = runner.next.next;
+            if(walker == runner){
                 return true;
             }
         }
