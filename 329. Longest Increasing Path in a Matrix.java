e5c9bcf40ef18b54d78a49e3503124f885f1082a
Yue <yuz82@pitt.edu>
2016-02-08 16:02:03 -0500

    Update 329. Longest Increasing Path in a Matrix.java

--- 329. Longest Increasing Path in a Matrix.java
+++ 329. Longest Increasing Path in a Matrix.java
@@ -24,6 +24,47 @@ Return 4
 The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed.
 */
 
+//using DP to store result for visited node
+public class Solution {
+    int max = 1;
+    int[][] matrix = null;
+    int m = 0, n = 0;
+    int[] x = {0,0,1,-1}; //up, down, right, left
+    int[] y = {1,-1,0,0};
+    int[][] visited = null;  
+    
+    public int longestIncreasingPath(int[][] matrix) {
+        int m = matrix.length;
+        if(matrix==null || m==0) { return 0; }
+        int n = matrix[0].length;
+        this.matrix = matrix;
+        this.m = m; this.n = n;
+        this.visited = new int[m][n];
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                max = Math.max(max, helper(i,j));
+            }
+        }
+        return max;
+    }
+    
+    public int helper(int i, int j){
+        if(visited[i][j]>0) { return visited[i][j]; }
+        int tmp = 1;
+        for(int k=0;k<4;k++){
+            int x1 = i + x[k], y1 = j + y[k];
+            if(x1>-1 && x1<m && y1>-1 && y1<n){
+                if(matrix[x1][y1] > matrix[i][j]){
+                    tmp = Math.max(helper(x1, y1)+1,tmp);
+                }
+            }
+        }
+        visited[i][j] = tmp;
+        return tmp;
+    }
+}
+
 
 //time limited exceed
 public class Solution {
