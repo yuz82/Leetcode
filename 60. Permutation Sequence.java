357aa1a1af471e026df70b3e2625b980bb301242
Yue <yuz82@pitt.edu>
2016-02-12 16:23:33 -0500

    Update 60. Permutation Sequence.java

--- 60. Permutation Sequence.java
+++ 60. Permutation Sequence.java
@@ -15,6 +15,24 @@ Given n and k, return the kth permutation sequence.
 Note: Given n will be between 1 and 9 inclusive.
 */
 
+//O(n)
+public String getPermutation(int n, int k) {
+    List<Integer> num = new LinkedList<Integer>();
+    for (int i = 1; i <= n; i++) num.add(i);
+    int[] fact = new int[n];  // factorial
+    fact[0] = 1;
+    for (int i = 1; i < n; i++) fact[i] = i*fact[i-1];  //store n!
+    k = k-1;
+    StringBuilder sb = new StringBuilder();
+    for (int i = n; i > 0; i--){
+        int ind = k/fact[i-1];
+        k = k%fact[i-1];
+        sb.append(num.get(ind));
+        num.remove(ind);
+    }
+    return sb.toString();
+}
+
 public class Solution {
     public String getPermutation(int n, int k) {
         List<Integer> list = new ArrayList<>();
