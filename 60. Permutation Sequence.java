87426dd386ccaf0932f0961533989a268f0a4b6a
Yue <yuz82@pitt.edu>
2016-02-12 16:00:00 -0500

    Update 60. Permutation Sequence.java

--- 60. Permutation Sequence.java
+++ 60. Permutation Sequence.java
@@ -19,31 +19,33 @@ Note: Given n will be between 1 and 9 inclusive.
 //time limited exceed solution
 public class Solution {
     public String getPermutation(int n, int k) {
-        ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
-        res.add(new ArrayList<Integer>());
-        for(int j=1;j<=n;j++){
+        ArrayList<String> res = new ArrayList<>();
+        int help = helper(n-1);
+        int first = (k-1) / help + 1;
+        int left = (k-1) % helper(n-1);
+        res.add("");
+        for(int i=1;i<=n;i++){
+            if(i==first) { continue; }
             int size = res.size();
-            for(;size>0;size--){
-                List<Integer> r = res.remove(0);
-                for(int i=0;i<=r.size();i++){
-                    List<Integer> tmp = new ArrayList<Integer>(r);
-                    tmp.add(i,j);
-                    res.add(tmp);
-                    if(j==n && res.size()==k){
-                        break;
-                    }
+            for(int j=0;j<size;j++){
+                String str = res.get(0);
+                int len = str.length();
+                res.remove(0);
+                for(int t=len;t>=0;t--){
+                    StringBuilder sb = new StringBuilder(str);
+                    sb.insert(t,i);
+                    res.add(sb.toString());
                 }
             }
         }
-        if(k>res.size()){
-            return "-1";
+        return first+res.get(left);
+    }
+    
+    public int helper(int n){
+        int res = 1;
+        for(int i=n;i>0;i--){
+            res *= i;
         }
-        List<Integer> tmp = res.get(k-1);
-        int len = tmp.size();
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<len;i++){
-            sb.append(tmp.get(i));
-        }
-        return sb.toString();
+        return res;
     }
 }
