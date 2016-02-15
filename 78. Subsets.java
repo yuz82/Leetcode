6ebc60109d9d6eaac774545306a54113dcad8e30
Yue <yuz82@pitt.edu>
2016-02-15 16:02:01 -0500

    Update 78. Subsets.java

--- 78. Subsets.java
+++ 78. Subsets.java
@@ -41,6 +41,26 @@ public class Solution {
     
 }
 
+//bit manipulation
+public class Solution {
+  public List<List<Integer>> subsets(int[] nums) {
+      int n = nums.length;
+      List<List<Integer>> subsets = new ArrayList<>();
+      for (int i = 0; i < Math.pow(2, n); i++)
+      {
+          List<Integer> subset = new ArrayList<>();
+          for (int j = 0; j < n; j++)
+          {
+              if (((1 << j) & i) != 0)
+                  subset.add(nums[j]);
+          }
+          Collections.sort(subset);
+          subsets.add(subset);
+      }
+      return subsets;
+  }
+}
+
 //combination
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
