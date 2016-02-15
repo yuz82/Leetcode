4607ff13ce68d54e2f638125633c722470c4345c
Yue <yuz82@pitt.edu>
2016-02-14 19:13:50 -0500

    Update 77. Combinations.java

--- 77. Combinations.java
+++ 77. Combinations.java
@@ -35,3 +35,53 @@ public class Solution {
         }
     }
 }
+
+
+//C(n,k)=C(n-1,k-1)+C(n-1,k)
+public class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        if (k == n || k == 0) {
+            List<Integer> row = new LinkedList<>();
+            for (int i = 1; i <= k; ++i) {
+                row.add(i);
+            }
+            return new LinkedList<>(Arrays.asList(row));
+        }
+        List<List<Integer>> result = this.combine(n - 1, k - 1);
+        result.forEach(e -> e.add(n));
+        result.addAll(this.combine(n - 1, k));
+        return result;
+    }
+}
+
+//dp and iterative
+public List<List<Integer>> combine(int n, int k) {
+    if(n < 1 || k < 1 || k > n)
+        return Collections.emptyList();
+
+    /** c(n, k) = c(n-1, k) + f(n, c(n-1, k-1)) **/
+    @SuppressWarnings("unchecked")
+    List<List<Integer>>[][] cache = new List[n + 1][k + 1];
+
+    /** init cache of k == 0 **/
+    for(int i = 0; i <= n; i++){
+        cache[i][0] = new ArrayList<>();
+        cache[i][0].add(new ArrayList<Integer>());
+    }
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i && j <= k; j++){
+            /** combine(i, j) **/
+            cache[i][j] = new ArrayList<>();
+            if(i - 1 >= j)
+                cache[i][j].addAll(cache[i - 1][j]);
+            for(List<Integer> list: cache[i - 1][j - 1]){
+                List<Integer> tmpList = new LinkedList<>(list);
+                tmpList.add(i);
+                cache[i][j].add(tmpList);
+            }
+        }
+    }
+
+    return cache[n][k];
+}
