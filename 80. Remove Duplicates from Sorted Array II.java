3469b51eaae2910f79d4158d5cb60769d587a99c
Yue <yuz82@pitt.edu>
2016-02-14 20:11:52 -0500

    Update 80. Remove Duplicates from Sorted Array II.java

--- 80. Remove Duplicates from Sorted Array II.java
+++ 80. Remove Duplicates from Sorted Array II.java
@@ -13,6 +13,21 @@ public class Solution {
     public int removeDuplicates(int[] nums) {
         int n = nums.length;
         if(nums==null || n==0) { return 0; }
+        int count = 2;
+        for(int i=2;i<n;i++){
+            if(nums[i]>nums[count-2]){
+                nums[count++] = nums[i];
+            }
+        }
+        return count;
+    }
+}
+
+//HashMap
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if(nums==null || n==0) { return 0; }
         int count = 0;
         HashMap<Integer, Integer> hm = new HashMap<>();
         for(int i=0;i<n;i++){
