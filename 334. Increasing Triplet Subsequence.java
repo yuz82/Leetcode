6583b1a478b4c6660cb9e22cf8a2a8022240d3e1
Yue <yuz82@pitt.edu>
2016-02-19 16:22:25 -0500

    Update 334. Increasing Triplet Subsequence.java

--- 334. Increasing Triplet Subsequence.java
+++ 334. Increasing Triplet Subsequence.java
@@ -14,7 +14,22 @@ Given [5, 4, 3, 2, 1],
 return false.
 */
 
-//O(n^2) solution
+//O(n) time and O(1) space solution
+public class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int n = nums.length;
+        if(nums==null || n==0){ return false; }
+        int small = Integer.MAX_VALUE, big = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            if(nums[i]<=small) { small = nums[i]; }
+            else if(nums[i]<=big) { big = nums[i]; }
+            else return true;
+        }
+        return false;
+    }
+}
+
+//O(n^2) time and O(n) space solution
 public class Solution {
     public boolean increasingTriplet(int[] nums) {
         int n = nums.length;
