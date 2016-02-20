689efb0bc3013261032e64b2944c71aa310c6021
Yue <yuz82@pitt.edu>
2016-02-20 16:32:15 -0500

    Update 289. Game of Life.java

--- 289. Game of Life.java
+++ 289. Game of Life.java
@@ -63,3 +63,47 @@ public class Solution {
         return count;
     }
 }
+
+
+//in-place 
+public class Solution {
+    public void gameOfLife(int[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        /* only need int 0-3
+        00: dead<-dead
+        01: dead<-live
+        10: live<-dead
+        11: live<-live
+        */
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int lives = count(board, i, j);
+                if(board[i][j]==1 && lives>=2 && lives<=3){
+                    board[i][j] = 3;
+                }
+                if(board[i][j]==0 && lives==3){
+                    board[i][j] = 2;
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                board[i][j] >>= 1;
+            }
+        }
+    }
+    
+    public int count(int[][] board, int i, int j){
+        int m = board.length;
+        int n = board[0].length;
+        int lives = 0;
+        for(int x = Math.max(i - 1, 0); x <= Math.min(i + 1, m - 1); x++) {
+            for(int y = Math.max(j - 1, 0); y <= Math.min(j + 1, n - 1); y++) {
+                lives += board[x][y] & 1;
+            }
+        }
+        lives -= board[i][j] & 1;
+        return lives;
+    }
+}
