d0078643168f0adb4d2382a32d116d371775c0aa
Yue <yuz82@pitt.edu>
2016-03-12 21:49:27 -0500

    Update Single Number II.java

--- Single Number II.java
+++ Single Number II.java
@@ -26,3 +26,12 @@ public class Solution {
         return -1;
     }
 }
+
+public int singleNumber(int[] A) {
+    int ones = 0, twos = 0;
+    for(int i = 0; i < A.length; i++){
+        ones = (ones ^ A[i]) & ~twos;
+        twos = (twos ^ A[i]) & ~ones;
+    }
+    return ones;
+}
