d610e0154f5d023b7c7ab9c5f220df2058c7bb4d
Yue <yuz82@pitt.edu>
2016-03-14 20:41:12 -0400

    Update 119 Pascal's Triangle II.java

--- 119 Pascal's Triangle II.java
+++ 119 Pascal's Triangle II.java
@@ -6,6 +6,20 @@ Note:
 Could you optimize your algorithm to use only O(k) extra space?
 */
 
+public List<Integer> getRow(int rowIndex) {
+    List<Integer> list = new ArrayList<Integer>();
+    if (rowIndex < 0)
+        return list;
+
+    for (int i = 0; i < rowIndex + 1; i++) {
+        list.add(0, 1);
+        for (int j = 1; j < list.size() - 1; j++) {
+            list.set(j, list.get(j) + list.get(j + 1));
+        }
+    }
+    return list;
+}
+
 public class Solution {
     public List<Integer> getRow(int rowIndex) {
         List<Integer> list = new ArrayList<Integer>();
