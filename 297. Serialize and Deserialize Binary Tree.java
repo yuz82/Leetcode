f4965742826980f4cee2a9f60b455ee67fd22043
Yue <yuz82@pitt.edu>
2016-03-19 12:04:12 -0400

    Create 297. Serialize and Deserialize Binary Tree.java

---
+++ 297. Serialize and Deserialize Binary Tree.java
@@ -0,0 +1,67 @@
+/*
+Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+For example, you may serialize the following tree
+
+    1
+   / \
+  2   3
+     / \
+    4   5
+as "[1,2,3,null,null,4,5]", just the same as how LeetCode OJ serializes a binary tree. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
+Note: Do not use class member/global/static variables to store states. Your serialize and deserialize algorithms should be stateless.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+    private static final String spliter = ",";
+    private static final String NULL = "#";
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        buildString(root, sb);
+        return sb.toString();
+    }
+    
+    public void buildString(TreeNode root, StringBuilder sb){
+        if(root==null){
+            sb.append(NULL).append(spliter);
+        }else{
+            sb.append(root.val).append(spliter);
+            buildString(root.left, sb);
+            buildString(root.right, sb);
+        }
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        Queue<String> queue = new LinkedList(Arrays.asList(data.split(spliter)));
+        return buildTree(queue);
+    }
+    
+    public TreeNode buildTree(Queue<String> queue){
+        String tmp = queue.poll();
+        if(tmp.equals(NULL)){ return null; }
+        else{
+            TreeNode root = new TreeNode(Integer.parseInt(tmp));
+            root.left = buildTree(queue);
+            root.right = buildTree(queue);
+            return root;
+        }
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.deserialize(codec.serialize(root));
