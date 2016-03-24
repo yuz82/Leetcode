b92dedc7d64aee27b3caf65e91febddb455ec4f1
Yue <yuz82@pitt.edu>
2016-03-24 11:11:24 -0400

    Update 78. Subsets.java

--- 78. Subsets.java
+++ 78. Subsets.java
@@ -41,6 +41,29 @@ public class Solution {
     
 }
 
+//recursive
+public List<List<Integer>> subsets(int[] S) {
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+    if(S.length == 0){
+        return result;
+    }
+
+    Arrays.sort(S);
+    dfs(S, 0, new ArrayList<Integer>(), result);
+    return result;
+}
+
+public void dfs(int[] s, int index, List<Integer> path, List<List<Integer>> result){
+    result.add(new ArrayList<Integer>(path));
+
+    for(int i = index; i < s.length; i++){
+        path.add(s[i]);
+        dfs(s, i+1, path, result);
+        path.remove(path.size()-1);
+    }
+}
+
 //bit manipulation
 public class Solution {
   public List<List<Integer>> subsets(int[] nums) {
