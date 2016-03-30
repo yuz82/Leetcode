8a266519d749da6f5f0dee3e2a67eafd374ae463
Yue <yuz82@pitt.edu>
2016-03-29 23:11:51 -0400

    Update 199. Binary Tree Right Side View.java

--- 199. Binary Tree Right Side View.java
+++ 199. Binary Tree Right Side View.java
@@ -21,7 +21,7 @@ You should return [1, 3, 4].
  * }
  */
  
- 
+//BFS
 public class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> res = new ArrayList();
@@ -38,5 +38,28 @@ public class Solution {
             }
         }
         return res;
+    }
+}
+
+
+//DFS
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        rightView(root, result, 0);
+        return result;
+    }
+
+    public void rightView(TreeNode curr, List<Integer> result, int currDepth){
+        if(curr == null){
+            return;
+        }
+        if(currDepth == result.size()){
+            result.add(curr.val);
+        }
+
+        rightView(curr.right, result, currDepth + 1);
+        rightView(curr.left, result, currDepth + 1);
+
     }
 }
