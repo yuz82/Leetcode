23398a0f5d6d9557a604526a2a97f03aaa05cf93
Yue <yuz82@pitt.edu>
2016-04-03 13:19:48 -0400

    Update 35 Search Insert Position.java

--- 35 Search Insert Position.java
+++ 35 Search Insert Position.java
@@ -10,6 +10,20 @@ Here are few examples.
 [1,3,5,6], 7 → 4
 [1,3,5,6], 0 → 0
 */
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length-1;
+        while(low+1 < high){
+            int mid = low + (high-low)/2;
+            if(nums[mid]>=target) { high = mid; }
+            else{ low = mid; }
+        }
+        if(nums[low]>=target) { return low; }
+        if(nums[high]>=target) { return high; }
+        return high+1;
+    }
+}
+
 
 public class Solution {
     public int searchInsert(int[] nums, int target) {
