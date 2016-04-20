997a8d33146393a280ca53c909e87da124356ff6
Yue <yuz82@pitt.edu>
2016-04-20 13:09:37 -0400

    Create 342. Power of Four.java

---
+++ 342. Power of Four.java
@@ -0,0 +1,21 @@
+/*
+Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+Example:
+Given num = 16, return true. Given num = 5, return false.
+
+Follow up: Could you solve it without loops/recursion?
+*/
+
+public class Solution {
+    public boolean isPowerOfFour(int num) {
+        return num>0 && (num==1 || (num%4==0 && isPowerOfFour(num/4)));
+    }
+}
+
+
+public class Solution {
+    public boolean isPowerOfFour(int num) {
+        return Integer.toString(num, 4).matches("10*");
+    }
+}
