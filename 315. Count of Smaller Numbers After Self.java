cd64443f07e9e3117a9b84bb275f1618201ddcd3
Yue <yuz82@pitt.edu>
2016-04-23 14:36:05 -0400

    Update 315. Count of Smaller Numbers After Self.java

--- 315. Count of Smaller Numbers After Self.java
+++ 315. Count of Smaller Numbers After Self.java
@@ -19,6 +19,40 @@ Traverse from the end of this array, for each element, using binary search to fi
 However, if using int[] to store the new array, although it has binarySearch() to simplify the code, the cost of insertion is hugh. Therefore, a binary search function implemented by arraylist is needed.
 */
 
+//not distinct number
+/*
+solution:  binary search
+Traverse from the end of this array, for each element, using binary search to find the location(return value plus one then negate it) to insert in a new empty array, the position to insert is the number of smaller numbers after itself.
+However, if using int[] to store the new array, although it has binarySearch() to simplify the code, the cost of insertion is hugh. Therefore, a binary search function implemented by arraylist is needed.
+*/
+public class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        int n = nums.length;
+        if(n==0){ return res; }
+        List<Integer> sorted = new ArrayList<>();
+        for(int i=n-1;i>=0;i--){
+            int index = binarySearch(sorted, nums[i]);
+            sorted.add(index, nums[i]);
+            res.add(0, index);
+        }
+        return res;
+    }
+    
+    public int binarySearch(List<Integer> list, int num){
+        if(list.size()==0) { return 0; }
+        int low = 0, high = list.size()-1;
+        while(low+1 < high){
+            int mid = low + (high - low)/2;
+            if(list.get(mid)>=num){ high = mid; }
+            else{ low = mid; }
+        }
+        if(list.get(low)>=num) { return low; }
+        else if(list.get(high)>=num) { return high; }
+        else{ return high+1; }
+    }
+}
+
 //time limit exceed 
 public class Solution {
     public List<Integer> countSmaller(int[] nums) {
