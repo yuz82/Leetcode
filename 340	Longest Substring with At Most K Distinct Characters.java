d60182d55bfa460343b966ff646bdb077c201c60
Yue <yuz82@pitt.edu>
2016-05-08 12:44:17 -0400

    Update 340	Longest Substring with At Most K Distinct Characters.java

--- 340	Longest Substring with At Most K Distinct Characters.java
+++ 340	Longest Substring with At Most K Distinct Characters.java
@@ -1,27 +1,26 @@
 
-//hashmap && queue  
+//hashmap   
 public static int lengthOfLongestSubstringKthDistinct(String str, int k){
         int n = str.length();
         if(str==null || n==0) { return 0; }
+        int max = 1;
         HashMap<Character, Integer> hm = new HashMap<>();
-        Queue<Character> queue = new LinkedList<>();
-        int max = 0;
+        int head = 0;
         for(int i=0;i<n;i++){
             char c = str.charAt(i);
-            queue.offer(c);
             if(hm.containsKey(c)){
                 int count = hm.get(c) + 1;
                 hm.put(c, count);
             }else{
                 while(hm.size()==k){
-                    char peek = queue.poll();
+                    char peek = str.charAt(head++);
                     int count = hm.get(peek) - 1;
                     if(count==0) { hm.remove(peek); }
                     else{ hm.put(peek, count); }
                 }
                 hm.put(c, 1);
             }
-            max = Math.max(max, queue.size());
+            max = Math.max(max, i-head+1);
         }
         return max;
     }
