ab57fe003e8ee73c95ca4d6a2905997d01fa6d66
Yue <yuz82@pitt.edu>
2016-05-08 12:42:23 -0400

    Update 159 Longest Substring with At Most Two Distinct Characters.java

--- 159 Longest Substring with At Most Two Distinct Characters.java
+++ 159 Longest Substring with At Most Two Distinct Characters.java
@@ -6,29 +6,28 @@ public class LongestSubstringwithAtMostTwoDistinctCharacters {
      * @param args the command line arguments
      */
     
-    //hashmap && queue    O(n) time
+    //hashmap    O(n) time
     public static int lengthOfLongestSubstringTwoDistinct(String str){
         int n = str.length();
         if(str==null || n==0) { return 0; }
+        int max = 1;
         HashMap<Character, Integer> hm = new HashMap<>();
-        Queue<Character> queue = new LinkedList<>();
-        int max = 0;
+        int head = 0;
         for(int i=0;i<n;i++){
             char c = str.charAt(i);
-            queue.offer(c);
             if(hm.containsKey(c)){
                 int count = hm.get(c) + 1;
                 hm.put(c, count);
             }else{
                 while(hm.size()==2){
-                    char peek = queue.poll();
+                    char peek = str.charAt(head++);
                     int count = hm.get(peek) - 1;
                     if(count==0) { hm.remove(peek); }
                     else{ hm.put(peek, count); }
                 }
                 hm.put(c, 1);
             }
-            max = Math.max(max, queue.size());
+            max = Math.max(max, i-head+1);
         }
         return max;
     }
