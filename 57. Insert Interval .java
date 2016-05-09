e606d101e2ae80c9c5e918fba32943a07def7066
Yue <yuz82@pitt.edu>
2016-05-09 18:08:04 -0400

    Update 57. Insert Interval .java

--- 57. Insert Interval .java
+++ 57. Insert Interval .java
@@ -12,7 +12,36 @@ Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10
 This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
 */
 
-
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        List<Interval> res = new ArrayList();
+        int i = 0, n = intervals.size();
+        //插入newInterval之前的
+        while(i<n && newInterval.start>intervals.get(i).end){
+            res.add(intervals.get(i++));
+        }
+        //与newInterval相比需要改动再插入的（newInterval在对比对象之后）
+        while(i<n && newInterval.start<=intervals.get(i).end && newInterval.end>=intervals.get(i).start){
+            newInterval = new Interval(Math.min(newInterval.start, intervals.get(i).start), Math.max(newInterval.end, intervals.get(i).end));
+            ++i;
+        }
+        res.add(newInterval);
+        //插入不需要对比之后的对象
+        while(i<n) {
+            res.add(intervals.get(i++));
+        }
+        return res;
+    }
+}
 
 //time limited exceed 
 public class Solution {
