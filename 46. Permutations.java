e2f29e67090d19b1818c2511590477f3b7f21ee5
Yue <yuz82@pitt.edu>
2016-05-13 19:47:55 -0400

    Update 46. Permutations.java

--- 46. Permutations.java
+++ 46. Permutations.java
@@ -26,3 +26,27 @@ public class Solution {
     }
     
 }
+
+
+
+//recursive
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList();
+        int n = nums.length;
+        if(nums==null || n==0) { return res; }
+        Arrays.sort(nums);
+        helper(nums, res, new ArrayList<Integer>());
+        return res;
+    }
+    
+    public void helper(int[] nums, List<List<Integer>> res, List<Integer> list){
+        if(list.size()==nums.length) { res.add(new ArrayList<Integer>(list)); return; }
+        for(int j=0;j<nums.length;j++){
+            if(list.contains(nums[j])) { continue; }
+            list.add(nums[j]);
+            helper(nums,res, list);
+            list.remove(list.size()-1);
+        }
+    }
+}
