70d2509b9b346df9682b2943beca177f7b7dae4a
Yue <yuz82@pitt.edu>
2016-05-15 14:22:56 -0400

    Update valid IP.java

--- valid IP.java
+++ valid IP.java
@@ -1,3 +1,4 @@
+//yapstone OA
 
 public static boolean validIP(String str) {
     return str.matches("((?:(?:25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d)))\\.){3}(?:25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d))))");
